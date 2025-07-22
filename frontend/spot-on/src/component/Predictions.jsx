5e5f02c2f3821dc60a8b39c8f0e11316b6b750d6
Christabel Gosiora Obi-Nwosu <christabelobi@meta.com>
2025-07-22 10:29:08 -0700

    Change in style and layout of predictions page and component

--- frontend/spot-on/src/component/Predictions.jsx
+++ frontend/spot-on/src/component/Predictions.jsx
@@ -4,164 +4,64 @@ import PredictionToggle from "./PredictionToggle";
 import BestAvailabilityCard from "./BestAvailabilityCard";
 import LotPredictionCard from "./LotPredictionCard";
 import MapLegend from "./MapLegend";
-const dummyLots = [
-  {
-    id: "lot1",
-    name: "Lot A",
-    predictions: {
-      now: {
-        availability: 85,
-        waitTime: "2 mins",
-        peak: "4:30 PM",
-        freeCount: 17,
-        totalSpots: 20,
-      },
-      15: {
-        availability: 78,
-        waitTime: "3 mins",
-        peak: "4:45 PM",
-        freeCount: 15,
-        totalSpots: 20,
-      },
-      30: {
-        availability: 60,
-        waitTime: "5 mins",
-        peak: "5:00 PM",
-        freeCount: 12,
-        totalSpots: 20,
-      },
-      50: {
-        availability: 35,
-        waitTime: "7 mins",
-        peak: "5:30 PM",
-        freeCount: 7,
-        totalSpots: 20,
-      },
-    },
-    lastReported: "2 mins ago",
-    walkTime: "3 mins",
-  },
-  {
-    id: "lot2",
-    name: "Lot B",
-    predictions: {
-      now: {
-        availability: 20,
-        waitTime: "7 mins",
-        peak: "5:15 PM",
-        freeCount: 4,
-        totalSpots: 20,
-      },
-      15: {
-        availability: 25,
-        waitTime: "6 mins",
-        peak: "5:30 PM",
-        freeCount: 5,
-        totalSpots: 20,
-      },
-      30: {
-        availability: 40,
-        waitTime: "5 mins",
-        peak: "5:45 PM",
-        freeCount: 8,
-        totalSpots: 20,
-      },
-      50: {
-        availability: 60,
-        waitTime: "4 mins",
-        peak: "6:00 PM",
-        freeCount: 12,
-        totalSpots: 20,
-      },
-    },
-    lastReported: "5 mins ago",
-    walkTime: "5 mins",
-  },
-  {
-    id: "lot3",
-    name: "Lot C",
-    predictions: {
-      now: {
-        availability: 55,
-        waitTime: "4 mins",
-        peak: "4:45 PM",
-        freeCount: 11,
-        totalSpots: 20,
-      },
-      15: {
-        availability: 45,
-        waitTime: "5 mins",
-        peak: "5:00 PM",
-        freeCount: 9,
-        totalSpots: 20,
-      },
-      30: {
-        availability: 30,
-        waitTime: "6 mins",
-        peak: "5:15 PM",
-        freeCount: 6,
-        totalSpots: 20,
-      },
-      50: {
-        availability: 20,
-        waitTime: "7 mins",
-        peak: "5:30 PM",
-        freeCount: 4,
-        totalSpots: 20,
-      },
-    },
-    lastReported: "1 min ago",
-    walkTime: "2 mins",
-  },
-];
-const Predictions = ({ setIsRoutingToHome, selectedSpot, searchKeyword }) => {
+import { clusterSpots } from "../utils/clusterSpots";
+import { use } from "react";
+
+const Predictions = ({ setIsRoutingToHome, spots }) => {
   const [selectedTime, setSelectedTime] = useState("now");
   const [lots, setLots] = useState([]);
-  const fetchForecast = async () => {
-    if (searchKeyword.length == 0) {
-      console.log("No spot selected");
-      return;
-    }
-    setLoading(true);
-    console.log("Fetching forecast for spot: ", selectedSpot.lotName);
+  const [bestLot, setBestLot] = useState(null);
+  const [otherLots, setOtherLots] = useState([]);
+
+  const fetchPredictions = async () => {
     try {
-      const lotName = selectedSpot.lotName;
-      const res = await fetch(`http://localhost:3000/predictions/${lotName}`);
+      const res = await fetch("http://localhost:3000/predictions");
       const data = await res.json();
-      if (data.error) {
-        throw new Error(data.error);
-      }
-      setForecastData(
-        data.map((entry) => ({
-          time: new Date(entry.time).toLocaleTimeString([], {
-            hour: "2-digit",
-            minute: "2-digit",
-          }),
-          availability: parseFloat(
-            (entry.predictedAvailability * 100).toFixed(1)
-          ),
-        }))
+      setLots(data);
+
+      const currentPredictions = data.map((lot) => ({
+        ...lot,
+        current: lot.predictions[selectedTime],
+      }));
+      const sortedLots = [...currentPredictions].sort(
+        (a, b) => b.current.availability - a.current.availability
       );
-      setLoading(false);
-    } catch (error) {
-      throw error;
+
+      setBestLot(sortedLots[0]);
+      setOtherLots(sortedLots.slice(1));
+    } catch (err) {
+      throw new Error(err);
     }
   };
   useEffect(() => {
-    // In real app, this is where we fetch prediction results based on time window
-    setLots(dummyLots); // Simulating load
+    try {
+      const clusteredLots = clusterSpots(spots);
+      const sendClusteredLots = async () => {
+        const response = await fetch(
+          "http://localhost:3000/predictions/clusters",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(clusteredLots),
+          }
+        );
+        const data = await response.json();
+        if (data.success) {
+          fetchPredictions();
+        } else {
+          throw new Error(data.error);
+        }
+      };
+      sendClusteredLots();
+    } catch (err) {
+      throw new Error(err);
+    }
   }, []);
-
-  const currentPredictions = dummyLots.map((lot) => ({
-    ...lot,
-    current: lot.predictions[selectedTime],
-  }));
-
-  const sortedLots = [...currentPredictions].sort(
-    (a, b) => b.current.availability - a.current.availability
-  );
-  const bestLot = sortedLots[0];
-  const otherLots = sortedLots.slice(1);
+  useEffect(() => {
+    fetchPredictions();
+  }, [selectedTime]);
   return (
     <div>
       <div className="route-header">
@@ -175,7 +75,6 @@ const Predictions = ({ setIsRoutingToHome, selectedSpot, searchKeyword }) => {
             }}
           ></i>
         </div>
-        <h2>SpotOn</h2>
         <div className="header-text">
           <p>Availabiloty Predictions for You</p>
           <p>Your spot, yur way</p>
@@ -185,13 +84,14 @@ const Predictions = ({ setIsRoutingToHome, selectedSpot, searchKeyword }) => {
         <h2 className="page-title">Prediction Dashboard</h2>
 
         <PredictionToggle onSelect={setSelectedTime} />
-
-        <BestAvailabilityCard
-          lotName={bestLot.name}
-          availability={bestLot.current.availability}
-          waitTime={bestLot.current.waitTime}
-          peakTime={bestLot.current.peak}
-        />
+        {bestLot && (
+          <BestAvailabilityCard
+            lotName={bestLot.name}
+            availability={bestLot.current.availability}
+            waitTime={bestLot.current.waitTime}
+            peakTime={bestLot.current.peak}
+          />
+        )}
 
         <div className="cards-section">
           {otherLots.map((lot) => (
